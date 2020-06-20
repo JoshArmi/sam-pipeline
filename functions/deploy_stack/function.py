c7ced2b347d0498306c7484b66178b24a9dec965
josharmi <josh.armitage@outlook.com>
2020-06-20 21:14:34 +0100

    Fixed S3 URL style and fixed the bucket template

--- functions/deploy_stack/function.py
+++ functions/deploy_stack/function.py
@@ -2,10 +2,11 @@ import os
 
 import boto3
 
-ROLE_NAME = os.environ['ROLE_NAME']
-STACK_NAME = os.environ['STACK_NAME']
 BUCKET_NAME = os.environ['BUCKET_NAME']
 BUCKET_PREFIX = os.environ['BUCKET_PREFIX']
+REGION = os.environ['REGION']
+ROLE_NAME = os.environ['ROLE_NAME']
+STACK_NAME = os.environ['STACK_NAME']
 
 session = boto3.Session()
 
@@ -14,8 +15,8 @@ def get_session(account_id):
     credentials = session.client('sts').assume_role(
         RoleArn=f'arn:aws:iam::{account_id}:role/CrossAccountDeploy',
         RoleSessionName='CrossAccountDeploy',
-    )
-    return session.Session(
+    )['Credentials']
+    return boto3.Session(
         aws_access_key_id=credentials["AccessKeyId"],
         aws_secret_access_key=credentials["SecretAccessKey"],
         aws_session_token=credentials["SessionToken"],
@@ -28,15 +29,15 @@ def handler(event, context):
     stacks = []
     paginator = cloudformation.get_paginator('list_stacks')
     for page in paginator.paginate():
-        stacks.extend(page['Stacks'])
+        stacks.extend(page['StackSummaries'])
     stack_names = [stack['StackName'] for stack in stacks]
     if STACK_NAME in stack_names:
         cloudformation.update_stack(
             StackName=STACK_NAME,
-            TemplateURL=f'{BUCKET_NAME}/{BUCKET_PREFIX}'
+            TemplateURL=f'https://{BUCKET_NAME}.s3-{REGION}.amazonaws.com/{BUCKET_PREFIX}'
         )
     else:
         cloudformation.create_stack(
             StackName=STACK_NAME,
-            TemplateURL=f'{BUCKET_NAME}/{BUCKET_PREFIX}'
+            TemplateURL=f'https://{BUCKET_NAME}.s3-{REGION}.amazonaws.com/{BUCKET_PREFIX}'
         )
