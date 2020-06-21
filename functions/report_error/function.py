c8bdc0f3006fee07056e8ab952b218e0740582bb
josharmi <josh.armitage@outlook.com>
2020-06-21 13:04:23 +0100

    Added publishing to SNS topic to report errors

--- functions/report_error/function.py
+++ functions/report_error/function.py
@@ -1,2 +1,14 @@
+import os
+
+import boto3
+
+TOPIC_ARN = os.environ['TOPIC_ARN']
+
+
 def handler(event, context):
-    print(event)
+    sns = boto3.Session().client('sns')
+    sns.publish(
+        TopicArn=TOPIC_ARN,
+        Subject='Error Deploying Stack',
+        Message=str(event),
+    )
